e6fd8de9c1f0e5fb0d09a3528ce64927aa406ac3
L00170150 <L00170150@student.lyit.ie>
2021-11-16 16:31:58 +0000

    initial files added.

---
+++ mavengithubaction/src/main/java/lyitlabs/ie/mavengithubaction/App.java
@@ -0,0 +1,13 @@
+package lyitlabs.ie.mavengithubaction;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
